7ed7efe6ec7c8794b0e16d61a6f776a0880652cc
nejaruli4460 <79055019+nejaruli4460@users.noreply.github.com>
2021-04-19 21:16:46 +0530

    done

--- script.js
+++ script.js
@@ -7,13 +7,29 @@ function dis(val){
 function solve()
 {
     let x=document.getElementById("result").value;
+    document.getElementById("history").value+=x;
     let y=eval(x);
+    document.getElementById("history").value+='='+y+' ';
     document.getElementById("result").value=y;
     document.getElementById("result").style.backgroundColor="#33FFCB";
     document.getElementById("result").style.color="white";
+
 }
 function clr()
 {
     document.getElementById("result").value="";
     document.getElementById("result").style.backgroundColor="#FFC933";
+}
+function hisclr(){
+    document.getElementById("history").value="";
+}
+function hist(){
+    document.getElementById("main").style.display="none";
+    document.getElementById("hist").style.display="block";
+
+}
+function back(){
+    document.getElementById("main").style.display="block";
+    document.getElementById("hist").style.display="none";
+
 }
