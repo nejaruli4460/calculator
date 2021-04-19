11f2003b447cdc80d073c03a4680711c27715ca7
nejaruli4460 <79055019+nejaruli4460@users.noreply.github.com>
2021-04-19 18:45:43 +0530

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+function dis(val){
+    let x=document.getElementById("result")
+    x.value+=val;
+    x.style.backgroundColor="white";
+    x.style.color="black";
+}
+function solve()
+{
+    let x=document.getElementById("result").value;
+    let y=eval(x);
+    document.getElementById("result").value=y;
+    document.getElementById("result").style.backgroundColor="black";
+    document.getElementById("result").style.color="white";
+}
+function clr()
+{
+    document.getElementById("result").value="";
+    document.getElementById("result").style.backgroundColor="tomato";
+}
